2086d3d2dd840233aa22a359bfb4f81b65efd612
Mohammed Hussain <mohammadhussain9924@gmail.com>
2026-06-01 06:42:55 -0400

    Initialize Spring Boot project

---
+++ src/main/java/com/mohammed/incidentpilot/IncidentpilotApplication.java
@@ -0,0 +1,13 @@
+package com.mohammed.incidentpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentpilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentpilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohammed/incidentpilot/IncidentpilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohammed.incidentpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentpilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
